756d475af13a649fd35d2219d6efcc0f83fdb0bc
Mykhail1387 <melnychukmykhailo87@gmail.com>
2019-10-23 21:29:58 +0300

    HW-task-01-05

---
+++ js/task-03.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const findLongestWord = function (string) {
+    const words = string.split(' ');
+    let longestWords = words[0];
+
+    for (const word of words) {
+        if (word.length > longestWords.length) {
+            longestWords = word;
+        }
+    }
+    return longestWords;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+
+console.log(findLongestWord('Google do a roll'));
+
+console.log(findLongestWord('May the force be with you'));
